108f42f0ce1c988be8519effe1e39409f968f8fb
Lin-DOS <26785782+Lin-DOS@users.noreply.github.com>
2024-09-06 05:54:12 +1000

    Add almost completed project

--- script.ts
+++ script.ts
@@ -3,10 +3,11 @@ const body = document.querySelector("body");
 const divContainer = document.querySelector("body");
 
 body.setAttribute("style", "min-height: 100vh; display: flex; flex-direction: column; align-items: center;")
-container.setAttribute("style", "width: 960px; display: flex; justify-content: center;");
+container.setAttribute("style", "width: 600px; display: flex; justify-content: center;");
 
 const btn = document.createElement("button");
 btn.textContent = "New grid";
+btn.setAttribute("style", "margin: 16px;")
 body.insertBefore(btn, container);
 const button = document.querySelector("button");
 createGrid(16);
@@ -15,19 +16,19 @@ function createGrid(size) {
 
   for (let i=0; i<size; i++) {
     const divContainer = document.createElement("div");
-    divContainer.setAttribute("style", "display: flex; flex-direction: column; height: 960px;");
+    divContainer.setAttribute("style", "display: flex; flex-direction: column; height: 600px;");
     container.appendChild(divContainer);
 
     const div = document.createElement("div");
     div.className = "divs"
-    div.setAttribute("style", "border: 1px solid black; aspect-ratio: 1/1; flex: 1 1 auto; ");
-    div.style.width = "calc((960px / " + size + ") - 2px)";
+    div.setAttribute("style", "border: 0px solid black; aspect-ratio: 1/1; flex: 1 1 auto;");
+    div.style.width = "calc((600px / " + size + ") - 2px)";
 
     for (let i=0; i<size; i++) {
       const div = document.createElement("div");
       div.className = "divs"
       div.setAttribute("style", "border: 1px solid black;  aspect-ratio: 1/1; flex: 1 1 auto;");
-      div.style.width = "calc((960px / " + size + ") - 2px)";
+      div.style.width = "calc((600px / " + size + ") - 2px)";
       divContainer.appendChild(div);
     }
   }
@@ -36,6 +37,10 @@ function createGrid(size) {
   divo.forEach((div) => {
     div.addEventListener("mouseover", () => {
     div.style.backgroundColor = "black";
+    if (div.style.opacity === "") {
+      div.style.opacity = "0.1";
+    }
+    div.style.opacity = "calc(" + div.style.opacity + " + 0.1)";
     });
   });
 }
@@ -49,13 +54,4 @@ button.addEventListener("click", () => {
     container.textContent = "";
     createGrid(num);
   }
-  
-});
-
-const divo = document.querySelectorAll("div.divs");
-
-divo.forEach((div) => {
-  div.addEventListener("mouseover", () => {
-  div.style.backgroundColor = "black";
-  });
 });
