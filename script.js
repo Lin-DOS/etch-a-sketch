36b946787df90049c64812bf45b600ba1543e524
Lin-DOS <26785782+Lin-DOS@users.noreply.github.com>
2024-09-09 15:18:57 +1000

    Add TypeScript

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
-// const div = document.createElement("div");
-const container = document.querySelector("div");
-// const p = document.createElement("p");
-// p.style.color = "red";
-// p.textContent = "I'm red!";
-// container.appendChild(p);
+var container = document.querySelector("div");
+var body = document.querySelector("body");
+var divContainer = document.querySelector("body");
+body.setAttribute("style", "min-height: 100vh; display: flex; flex-direction: column; align-items: center;");
+container.setAttribute("style", "width: 600px; display: flex; justify-content: center;");
+var btn = document.createElement("button");
+btn.textContent = "New grid";
+btn.setAttribute("style", "margin: 16px;");
+body.insertBefore(btn, container);
+var button = document.querySelector("button");
+createGrid(16);
 
-
-for (i=0; i<=16; i++) {
-    const div = document.createElement("div");
-    div.setAttribute("style", "border: 1px solid black; width: ; height: 64px;");
-    // div.textContent = "div";
-
-    container.setAttribute("style", "display: flex; flex-wrap: wrap;");
-    container.appendChild(div);
+function createGrid(size) {
+    for (var i = 0; i < size; i++) {
+        var divContainer_1 = document.createElement("div");
+        divContainer_1.setAttribute("style", "display: flex; flex-direction: column; height: 600px;");
+        container.appendChild(divContainer_1);
+        for (var j = 0; j < size; j++) {
+            var div = document.createElement("div");
+            div.className = "divs";
+            div.setAttribute("style", "border: 1px solid black;  aspect-ratio: 1/1; flex: 1 1 auto;");
+            div.style.width = "calc((600px / " + size + ") - 2px)";
+            divContainer_1.appendChild(div);
+        }
+    }
+    document.querySelectorAll("div.divs").forEach(function (div) {
+        div.addEventListener("mouseover", function () {
+            div.style.backgroundColor = "black";
+            if (div.style.opacity === "") {
+                div.style.opacity = "0.1";
+            }
+            div.style.opacity = "calc(" + div.style.opacity + " + 0.1)";
+        });
+    });
 }
+
+button.addEventListener("click", function () {
+    var userInput = prompt("Enter number of squares in grid: ");
+    var num = parseInt(userInput);
+    if (num < 1 || num > 50) {
+        alert("Can't create grids larger than 50x50! Enter a number between 1 and 50.");
+    }
+    else {
+        container.textContent = "";
+        createGrid(num);
+    }
+});
--- src/script.ts
+++ src/script.ts
@@ -1,11 +1,11 @@
-const container = document.querySelector("div");
-const body = document.querySelector("body");
+const container: HTMLDivElement | null = document.querySelector("div");
+const body: HTMLBodyElement = document.querySelector("body")!;
 const divContainer = document.querySelector("body");
 
 body.setAttribute("style", "min-height: 100vh; display: flex; flex-direction: column; align-items: center;")
 container.setAttribute("style", "width: 600px; display: flex; justify-content: center;");
 
-const btn = document.createElement("button");
+const btn: HTMLElement = document.createElement("button");
 btn.textContent = "New grid";
 btn.setAttribute("style", "margin: 16px;")
 body.insertBefore(btn, container);
@@ -14,27 +14,21 @@ createGrid(16);
 
 function createGrid(size) {
 
-  for (let i=0; i<size; i++) {
+  for (let i: number = 0; i < size; i++) {
     const divContainer = document.createElement("div");
     divContainer.setAttribute("style", "display: flex; flex-direction: column; height: 600px;");
     container.appendChild(divContainer);
 
-    const div = document.createElement("div");
-    div.className = "divs"
-    div.setAttribute("style", "border: 0px solid black; aspect-ratio: 1/1; flex: 1 1 auto;");
-    div.style.width = "calc((600px / " + size + ") - 2px)";
-
-    for (let i=0; i<size; i++) {
+    for (let j: number = 0; j < size; j++) {
       const div = document.createElement("div");
-      div.className = "divs"
+      div.className = "divs";
       div.setAttribute("style", "border: 1px solid black;  aspect-ratio: 1/1; flex: 1 1 auto;");
       div.style.width = "calc((600px / " + size + ") - 2px)";
       divContainer.appendChild(div);
     }
   }
 
-  const divo = document.querySelectorAll("div.divs");
-  divo.forEach((div) => {
+  document.querySelectorAll("div.divs").forEach((div: HTMLElement) => {
     div.addEventListener("mouseover", () => {
     div.style.backgroundColor = "black";
     if (div.style.opacity === "") {
@@ -46,9 +40,10 @@ function createGrid(size) {
 }
 
 button.addEventListener("click", () => {
-  let num = prompt("Enter number of squares in grid: ");
-  if (num > 100) {
-    alert("Can't create grids larger than 100x100! Enter a number below 100.");
+  let userInput: string = prompt("Enter number of squares in grid: ");
+  let num: number = parseInt(userInput);
+  if (num < 1 || num > 50) {
+    alert("Can't create grids larger than 50x50! Enter a number between 1 and 50.");
   }
   else {
     container.textContent = "";
